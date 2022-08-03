e3c5e7c5b9d4717f6cc9052b7f41a92c8b5e9bea
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-08-03 12:46:44 -0500

    fixing settings

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,9 @@ export default {
 </script>
 
 <template>
-  <div v-if="flashMessage" class="alert alert-success" v-on:click="flashMessage = null">{{ flashMessage }}</div>
+  <div v-if="flashMessage" class="alert alert-success" v-on:click="flashMessage = null">
+    {{ flashMessage }}
+  </div>
   <header id="header" class="d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
       <h1 class="logo"><a href="/">Fight Night Tracker</a></h1>
